71e6fce791364f8fb51b1ad1963283b1fbebbf27
JiaQi Xu <47347516+bubbliiiing@users.noreply.github.com>
2019-11-20 21:25:45 +0800

    Update train.py

--- deeplab_Mobile/train.py
+++ deeplab_Mobile/train.py
@@ -109,7 +109,6 @@ if __name__ == "__main__":
             metrics = ['accuracy'])
     batch_size = 4
     print('Train on {} samples, val on {} samples, with batch size {}.'.format(num_train, num_val, batch_size))
-    model.load_weights("logs/ep012-loss0.066-val_loss0.124.h5")
     # 开始训练
     model.fit_generator(generate_arrays_from_file(lines[:num_train], batch_size),
             steps_per_epoch=max(1, num_train//batch_size),
